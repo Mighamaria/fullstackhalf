203daf5aeb126058086cf7ad630143a437220da5
Mighamaria <mighka.maria2@gamil.com>
2023-04-03 18:16:42 +0530

    fullstack

--- ust-emp/src/app/employe-reg.service.ts
+++ ust-emp/src/app/employe-reg.service.ts
@@ -10,7 +10,7 @@ export class EmployeRegService {
   constructor(private http:HttpClient) { }
 
   public doRegistration(user: any){
-    return this.http.post("http://localhost:9090/register",user,{responseType:'text' as 'json'});
+    return this.http.post("http://localhost:8086/registration/addemps",user,{responseType:'text' as 'json'});
   }
   public getUsers(){
     return this.http.get("http://localhost:9090/getAllUsers");
--- ust-emp/src/app/employee-reg/employee-reg.component.ts
+++ ust-emp/src/app/employee-reg/employee-reg.component.ts
@@ -3,25 +3,25 @@ import { EmployeRegService } from '../employe-reg.service';
 import { User } from '../User';
 
 @Component({
-  selector: 'app-employee-reg',
+  selector: 'app-menu-card',
   templateUrl: './employee-reg.component.html',
   styleUrls: ['./employee-reg.component.css']
 })
-export class EmployeeRegComponent  implements OnInit{
-  
-    user:User=new User("","",0,"");
+export class EmployeeRegComponent implements OnInit{
+  user:User=new User("","",0,"");
   message: any;
     constructor(private service:EmployeRegService){
       
     }
-   ngOnInit(){
-    
-   } 
-public registerNow(){
-  let resp=this.service.doRegistration(this.user);
-  resp.subscribe(data=>this.message=data);
-}
-    
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnChanges(){
+
+  }
+  public registerNow(){
+    let resp=this.service.doRegistration(this.user);
+    resp.subscribe(data=>this.message=data);
   }
 
-
+}
